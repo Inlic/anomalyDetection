9de21c6c967ec867afc08a477f3f975ba82f592d
Inlic <bwiedeback@gmail.com>
2021-02-04 16:01:03 -0700

    graphing tests

--- anomalyDetection.py
+++ anomalyDetection.py
@@ -15,33 +15,63 @@ bruce_data.head()
 bruce_data_copy = bruce_data.copy()
 bruce_data_copy.head()
 
-#visualize the data
-
-bruce_data.describe()
-
-bruce_data_copy[["Asset1","Asset2"]].agg(['min','max','mean','std']).round(decimals=2)
-
-fig, ax = plot.subplots(1,1)
-# bruce_data_copy[["Asset1","Asset2"]].kde(ax=ax, legend=False,title='Histogram: Asset1 vs Asset2')
-# Histograms of the data
-bruce_data_copy[["Asset1","Asset2"]].plot.hist(grid=True, bins=100, ax=ax)
-
-# Creating a histogram of a normal distribution with mean 10 and std 1.
-dist = norm(loc=10, scale=1).rvs(size=2000)
-x = np.linspace(0,20,num=100)
-
-# Dropping all the plots on top of each other.
-ax.hist(dist, bins=100)
-
-
-####
+# #visualize the data
+#
+# bruce_data.describe()
+#
+# bruce_data_copy[["Asset1","Asset2"]].agg(['min','max','mean','std']).round(decimals=2)
+#
+# fig, ax = plot.subplots(1,1)
+# # bruce_data_copy[["Asset1","Asset2"]].kde(ax=ax, legend=False,title='Histogram: Asset1 vs Asset2')
+# # Histograms of the data
+# bruce_data_copy[["Asset1","Asset2"]].plot.hist(grid=True, bins=100, ax=ax)
+#
+# # Creating a histogram of a normal distribution with mean 10 and std 1.
+# dist = norm(loc=10, scale=1).rvs(size=2000)
+# x = np.linspace(0,20,num=100)
+#
+# # Dropping all the plots on top of each other.
+# ax.hist(dist, bins=100)
+#
+# ####
 
 # data1 = data.copy()
-# data1['day'] = [i.weekday() for i in data.ds]
-# data1['month'] = data1.ds.dt.month
+bruce_data_copy['day'] = [i.weekday() for i in bruce_data.DATE]
+bruce_data_copy['month'] = bruce_data_copy.DATE.dt.month
 # data1.head()
 
 # alt + shift + e for batch statements in python console
+
+clf = IsolationForest(max_samples=100, contamination=0.05)
+clf.fit(bruce_data_copy[['Asset1','month']])
+scores1 = clf.decision_function(bruce_data_copy[['Asset1','month']])
+asset1AnomalyMean = sum(scores1)/len(scores1)
+
+y_pred_train = clf.predict(bruce_data_copy[['Asset1','month']])
+
+ax = bruce_data.iloc[y_pred_train == 1].plot(x='DATE', y='Asset1',kind='scatter',figsize=(15,8))
+bruce_data.iloc[y_pred_train == -1].plot(x='DATE', y='Asset1', kind='scatter', color='red', ax=ax)
+plot.title("Anomalies w/Isolation Forests Asset 1")
+plot.show()
+
+
+
+#############################  Anomaly Scores Lower = more abnormal
+
+clf = IsolationForest(max_samples=100, contamination=0.05)
+clf.fit(bruce_data_copy[['Asset2','month']])
+scores2 = clf.decision_function(bruce_data_copy[['Asset2','month']])
+asset2AnomalyMean = sum(scores2)/len(scores2)
+
+
+y_pred_train = clf.predict(bruce_data_copy[['Asset2','month']])
+
+ax = bruce_data.iloc[y_pred_train == 1].plot(x='DATE', y='Asset2',kind='scatter',figsize=(15,8))
+bruce_data.iloc[y_pred_train == -1].plot(x='DATE', y='Asset2', kind='scatter', color='red', ax=ax)
+plot.title("Anomalies w/Isolation Forests Asset 2")
+plot.show()
+
+len(bruce_data.iloc[y_pred_train == 1])
 
 # clf = IsolationForest(max_samples=100, contamination=.5)
 # clf.fit(data1[['y', 'month']])
