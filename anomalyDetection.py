e1a74518e9aee068b0f0ee988bfc89f169ff7a6b
Inlic <bwiedeback@gmail.com>
2021-02-04 11:10:11 -0700

    matplot isolation forest

--- anomalyDetection.py
+++ anomalyDetection.py
@@ -12,3 +12,13 @@ data1 = data.copy()
 data1['day'] = [i.weekday() for i in data.ds]
 data1['month'] = data1.ds.dt.month
 data1.head()
+
+clf = IsolationForest(max_samples=100, contamination='auto')
+clf.fit(data1[['y', 'month']])
+
+y_pred_train = clf.predict(data1[['y','month']])
+
+ax = data.iloc[y_pred_train == 1].plot(x='ds', y='y',kind='scatter',figsize=(15,8))
+data.iloc[y_pred_train == -1].plot(x='ds', y='y', kind='scatter', color='red', ax=ax)
+plot.title("Anomalies w/Isolation Forests")
+plot.show()
